11736c2ae41ff7f9d88424581f3db7b0d799a6eb
estevamponte <tecoponte@gmail.com>
2020-03-17 19:22:50 -0300

    Adicionando associacao com product para user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
+
+  it { is_expected.to have_many(:product).dependent(:destroy) }
+
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to allow_value('tecoponte@gmail.com').for(:email) }
